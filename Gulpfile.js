e8ee68b6bafaac0cc2f62f44fbaacbb15586f791
Florian Migot <florian.migot@gmail.com>
2015-11-17 17:51:39 +0100

    Creation of the tab icon + font-inline implemented ref:WEBDEV-1366

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,10 +6,20 @@ var autoprefixer = require('gulp-autoprefixer');
 var swig = require('gulp-swig');
 var runSequence = require('run-sequence');
 var del = require('del');
+var iconfont = require('gulp-iconfont');
+var consolidate = require("gulp-consolidate");
+var rename = require('gulp-rename');
+var base64 = require('gulp-base64');
 
 gulp.task('css', function () {
     return gulp.src('sandbox/**/*.scss')
         .pipe(sass())
+        .pipe(base64({
+            baseDir: 'assets/scss',
+            extensions: ['woff'],
+            maxImageSize: 500000,
+            debug: true
+        }))
         .pipe(autoprefixer())
         .pipe(pixrem())
         .pipe(gulp.dest('build/sandbox'))
@@ -24,6 +34,44 @@ gulp.task('html', function () {
 gulp.task('fonts', function () {
     return gulp.src(['assets/fonts/*.*'])
         .pipe(gulp.dest('build/fonts'))
+});
+
+gulp.task('iconfont', function () {
+    return gulp.src([
+            'assets/icons/common/astro/*.svg',
+            'assets/icons/common/meteo/*.svg',
+            'assets/icons/common/astro/*.svg',
+            'assets/icons/common/others/*.svg',
+            'assets/icons/common/social/facebook/*.svg',
+            'assets/icons/common/social/googleplus/*.svg',
+            'assets/icons/common/social/instagram/*.svg',
+            'assets/icons/common/social/linkedin/*.svg',
+            'assets/icons/common/social/pinterest/*.svg',
+            'assets/icons/common/social/twitter/*.svg',
+            'assets/icons/common/social/youtube/*.svg',
+            'assets/icons/funradio/*.svg',
+            'assets/icons/girls/*.svg',
+            'assets/icons/rtl/*.svg',
+            'assets/icons/rtl2/*.svg'
+        ])
+        .pipe(iconfont({
+            fontName: 'Kawabunga-Icon',
+            normalize: true,
+            svg: true
+
+        }))
+        .on('glyphs', function (glyphs, options) {
+            gulp.src('assets/icons/_fonticon.scss.template')
+                .pipe(consolidate('lodash', {
+                    glyphs: glyphs,
+                    fontName: 'Kawabunga-Icon',
+                    fontPath: '../fonts/',
+                    className: 'icon'
+                }))
+                .pipe(rename('_fonticon.scss'))
+                .pipe(gulp.dest('assets/scss/'));
+        })
+        .pipe(gulp.dest('assets/fonts/'))
 });
 
 gulp.task('clean', function (cb) {
