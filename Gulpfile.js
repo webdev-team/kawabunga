c5f06cd703628d9913cd0b4f27dcc9d70ddc0a60
Florian Migot <florian.migot@gmail.com>
2015-12-31 15:09:17 +0100

    Huge mofif of architecture + new components ref:WEBDEV-1366

--- Gulpfile.js
+++ Gulpfile.js
@@ -130,7 +130,7 @@ gulp.task('iconfont', function () {
                     className: 'icon'
                 }))
                 .pipe(rename('_fonticon.scss'))
-                .pipe(gulp.dest('assets/scss/'));
+                .pipe(gulp.dest('assets/scss/partial/'));
         })
         .pipe(gulp.dest('assets/font/'))
 });
