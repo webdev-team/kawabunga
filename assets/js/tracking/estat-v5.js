415c56e4760e8a6a51ee17f866d9ae1f800835c3
Damien Bourdette <damien.bourdette@rtlnet.fr>
2016-06-10 13:05:33 +0200

    Added estat-v5.js

--- assets/js/tracking/estat-v5.js
+++ assets/js/tracking/estat-v5.js
@@ -1,5 +1,4 @@
 var forEach = require('lodash/forEach');
-var head = require('lodash/head');
 var scriptLoader = require('../utils/script-loader');
 
 /**
@@ -13,20 +12,20 @@ var SCRIPT_INTEGRATION = '//prof.estat.com/js/mu-integration-5.1.js';
 var contentTag = null;
 
 exports.init = function() {
-    var tag = head(document.querySelector('[data-role="estat"]'));
+    var tag = document.querySelector('[data-role="estat"]');
 
     if (tag) {
         scriptLoader.ensureLoaded(SCRIPT).then(function() {
             contentTag = new eStatTag({
-                serial: tag.attr('data-serial'),
+                serial: tag.getAttribute('data-serial'),
                 measure:"page"
             });
 
             exports.post({
-                level_1: tag.attr('data-level1'),
-                level_2: tag.attr('data-level2'),
-                level_3: tag.attr('data-level3'),
-                level_4: tag.attr('data-level4')
+                level_1: tag.getAttribute('data-level1'),
+                level_2: tag.getAttribute('data-level2'),
+                level_3: tag.getAttribute('data-level3'),
+                level_4: tag.getAttribute('data-level4')
             });
         });
     }
