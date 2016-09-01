92b9061bf4642439aed36d9b76c1ed7c3b2063a2
merkizz <thomas.lay@rtlnet.fr>
2016-09-01 12:00:32 +0200

    Add vuesjs component (toaster)

---
+++ assets/vuejs/component/Toaster.vue
@@ -0,0 +1,57 @@
+<style lang="sass" scoped>
+    .toaster {
+        display: table;
+        position: fixed;
+        bottom: 0;
+        z-index: 10;
+        background: #fff;
+        height: 100px;
+        transition: transform 1s;
+        transform: translate(0, 200px);
+        &.active {
+            /*bottom: 0;*/
+            transform: translate(0, 0);
+        }
+
+        .message {
+            display: table-cell;
+            vertical-align: middle;
+        }
+    }
+
+
+</style>
+
+<template>
+
+    <div class="well well-lg toaster" :class="{active: active}">
+        <div class="message">{{ message }}</div>
+    </div>
+
+</template>
+
+<script>
+    var Vue = require('vue');
+
+    module.exports = {
+        data: function() {
+            return {
+                active: false,
+                message: ''
+            }
+        },
+        methods: {
+            show: function (msg) {
+                if(typeof msg !== 'string' || msg.length === 0) {
+                    return;
+                }
+                var self = this;
+
+                self.message = msg;
+                self.active = true;
+
+                setTimeout(function(){  self.active = false; }, 4000);
+            }
+        }
+    }
+</script>
