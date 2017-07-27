e7cce330be09bc22b9e5f2c3be1444749c03a6f0
Olivier Mobili <olivier.mobili@rtlnet.fr>
2017-07-27 16:48:37 +0200

    WEBDEV-2977 estat-script unit test

---
+++ tests-jest/js/tracking/estat-script.test.ts
@@ -0,0 +1,26 @@
+// ..
+// Imports
+//
+
+import * as env from '../../test-env';
+import * as estat from '../../../assets/js/tracking/estat-script.js';
+
+// ..
+// Unit Tests
+//
+
+describe('estat-script.js', function () {
+    beforeEach(function() {
+        env.initWithHtml('<div><script></script></div>');
+    });
+
+    it('should setup estat marker', function () {
+        estat.insertScriptWithSerial('219019211308' /* rtl serial */);
+
+        expect(global._PJS).toBe(0);
+        expect(global._cmsJS).toBe(0);
+        expect(global._eStatDS).toBe(1);
+        expect(global.eStat_PJS).not.toBeNull();
+        expect(global.eStat_CMSJS).not.toBeNull();
+    });
+});
