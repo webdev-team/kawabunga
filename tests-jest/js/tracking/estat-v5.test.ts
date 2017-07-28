ce1839fcd460de9bf006871859543de225ce291d
Olivier Mobili <olivier.mobili@rtlnet.fr>
2017-07-28 16:34:34 +0200

    WEBDEV-2977 add tests

--- tests-jest/js/tracking/estat-v5.test.ts
+++ tests-jest/js/tracking/estat-v5.test.ts
@@ -19,13 +19,11 @@ describe('estat-script.js', function () {
         env.initWithHtml('<div></div>');
     });
     afterEach(() => {
-        spyScriptLoader.mockReset();
+        spyScriptLoader.mockRestore();
     });
 
     describe('ensureLoaded', function () {
         test('should load estat script', done => {
-            //scriptLoaderMock.expects("ensureLoaded").once().returns(new Promise(resolve => resolve()))
-
             estatV5.ensureLoaded().then(() => {
                 expect(spyScriptLoader).toHaveBeenCalledTimes(1);
                 done();
@@ -33,7 +31,74 @@ describe('estat-script.js', function () {
         });
 
         test('should pass estat module in resolve function', (done) => {
-            done();
+            estatV5.ensureLoaded().then((module) => {
+                expect(spyScriptLoader).toHaveBeenCalledTimes(1);
+                expect(module).toBe(estatV5);
+                done();
+            });
+        });
+    });
+
+    describe('readPageConfig', () => {
+        test('should read page tag from html', () => {
+            env.initWithHtml('<div data-role="estat" data-serial="123" data-level1="level1" data-level2="level2" data-level3="level3" data-level4="level4"></div>');
+
+            expect(estatV5.readPageConfig()).toMatchObject({
+                serial: '123',
+                measure: 'page',
+                levels: {
+                    level_1: 'level1',
+                    level_2: 'level2',
+                    level_3: 'level3',
+                    level_4: 'level4'
+                }
+            });
+        });
+
+        test('should read page tag with crmID from html', () => {
+            env.initWithHtml('<div data-role="estat" data-serial="123" data-level1="level1" data-level2="level2" data-level3="level3" data-level4="level4" data-crmID="idxxx"></div>');
+
+            expect(estatV5.readPageConfig()).toMatchObject({
+                serial: '123',
+                measure: 'page',
+                levels: {
+                    level_1: 'level1',
+                    level_2: 'level2',
+                    level_3: 'level3',
+                    level_4: 'level4'
+                },
+                crmID: 'idxxx'
+            });
+        });
+    });
+
+    describe('createEstatTag', () => {
+        test('should create estat stream tag', done => {
+            global.eStatTag = class eStatTag {
+                constructor(config) {
+                    expect(config.serial).toBe(241041208720);
+                    expect(config.measure).toBe('streaming');
+
+                    done();
+                }
+            };
+
+            let votreCallbackDePosition = () => {};
+            let playerObject = {};
+
+            estatV5.createEstatTag({
+                serial: 241041208720,
+                measure: "streaming",
+                streaming: {
+                    diffusion: "replay",
+                    callbackPosition: votreCallbackDePosition,
+                    playerObj: playerObject,
+                    playerName: "player",
+                    streamName: "JT-20141106",
+                    streamDuration: "6000"
+                },
+                levels: {level_1: "Film", level_2: "Cinéma"}
+            });
         });
     });
 });
