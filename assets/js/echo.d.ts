17fd851d6c8fb3386f6d6e4e2428dda37e93b00d
Damien Bourdette <damien.bourdette@rtlnet.fr>
2018-05-16 17:50:23 +0200

    Remove echo module (used to test ts modules in kawa)

--- assets/js/echo.d.ts
+++
@@ -1,2 +0,0 @@
-export declare let mesage: string;
-export declare let compute: (name: string) => string;
--- assets/ts/echo.ts
+++
@@ -1,9 +0,0 @@
-// test file to test generation of js and d.ts files
-
-let variable = 2;
-
-export let mesage = 'really ?';
-
-export let compute = function (name: string): string {
-    return 'Hello ' + name;
-}
