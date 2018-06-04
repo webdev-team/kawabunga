ffceb002304975368ea2464c7e15a90e2bd70c09
Damien Bourdette <damien.bourdette@rtlnet.fr>
2018-06-04 14:37:32 +0200

    fix taoster path

--- sandbox/js/components.js
+++ sandbox/js/components.js
@@ -1,5 +1,5 @@
 
-var Toaster = require('../../assets/js/widget/Toaster.js').default;
+var Toaster = require('../../assets/js/widget/toaster.js').default;
 
 document.getElementById('toaster-show-button').addEventListener('click', function() {
     new Toaster('Un joli toaster').show();
