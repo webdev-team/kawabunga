d0fb54e75cac73675c7cf2d7ff3fca0f53712a6f
aleborgne <alex.e.leb@gmail.com>
2018-07-03 13:41:55 +0200

    tsc compiled files

--- assets/js/advertising/load-ligatus.js
+++ assets/js/advertising/load-ligatus.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var scriptLoader = require("../../js/utils/script-loader");
 var $ = require("../../js/utils/dom");
+var cnil_1 = require("../cnil/cnil");
+var cnil_cookie_1 = require("../cnil/cnil-cookie");
 exports.init = function () {
+    if (cnil_1.cnil.isOff(cnil_cookie_1.ADS)) {
+        return;
+    }
     // loads ligatus, yahoo gemini... using data-role="load-script" selector
     scriptLoader.getScriptsToLoad().forEach(scriptLoader.ensureLoaded);
     $('.js-async').forEach(function (item) {
