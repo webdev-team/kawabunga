e55a5e03fbc5bc43edab7c6cbf36871bbce6fd7a
aleborgne <alex.e.leb@gmail.com>
2019-08-01 17:06:21 +0200

    refactor didomi CMP + add event listeners - WEBDEV-4475

--- assets/ts/cmp/didomi.ts
+++ assets/ts/cmp/didomi.ts
@@ -1,12 +1,25 @@
 import * as env from '../env/env';
 import * as scriptLoader from '../../js/utils/script-loader.js';
+import {Observable} from "../utils/observable";
 
 declare global {
     interface Window {
         gdprAppliesGlobally: any;
         didomiConfig: any;
+        didomiEventListeners: Array<any>;
+        Didomi: any;
     }
 }
+
+export enum Purpose {
+    COOKIE = "cookies",
+    AD_PERSONALIZATION = "advertising_personalization",
+    CONTENT_PERSONALIZATION = "content_personalization",
+    ANALYTICS = "analytics",
+    AD_DELIVERY = "ad_delivery"
+}
+
+let consentChanged$ = new Observable<object>();
 
 let getThemeColor = function(): string {
     let themeColor: string;
@@ -25,15 +38,21 @@ let getThemeColor = function(): string {
     return themeColor;
 };
 
-export let init = function()  {
+export let init = function(): void {
 
     window.gdprAppliesGlobally = true;
     // @ts-ignore
     (function(){function n(){if(!window.frames.__cmpLocator){if(document.body&&document.body.firstChild){var e=document.body;var t=document.createElement("iframe");t.style.display="none";t.name="__cmpLocator";e.insertBefore(t,e.firstChild)}else{setTimeout(n,5)}}}function e(e,t,n){if(typeof n!=="function"){return}if(!window.__cmpBuffer){window.__cmpBuffer=[]}if(e==="ping"){n({gdprAppliesGlobally:window.gdprAppliesGlobally,cmpLoaded:false},true)}else{window.__cmpBuffer.push({command:e,parameter:t,callback:n})}}e.stub=true;function t(a){if(!window.__cmp||window.__cmp.stub!==true){return}if(!a.data){return}var r=typeof a.data==="string";var e;try{e=r?JSON.parse(a.data):a.data}catch(t){return}if(e.__cmpCall){var i=e.__cmpCall;window.__cmp(i.command,i.parameter,function(e,t){var n={__cmpReturn:{returnValue:e,success:t,callId:i.callId}};a.source.postMessage(r?JSON.stringify(n):n,"*")})}}if(typeof window.__cmp!=="function"){window.__cmp=e;if(window.addEventListener){window.addEventListener("message",t,false)}else{window.attachEvent("onmessage",t)}}n()})();
 
+    window.didomiConfig = didomiConfig();
+
+    scriptLoader.ensureLoaded('https://sdk.privacy-center.org/loader.js', attachEventListeners);
+};
+
+let didomiConfig = function(): object {
     let themeColor = getThemeColor();
 
-    window.didomiConfig = {
+    return {
         app: {
             apiKey: '<Your API key>',
             vendors: {
@@ -69,13 +88,23 @@ export let init = function()  {
                 }
             }
         }
-    };
-
-    scriptLoader.ensureLoaded('https://sdk.privacy-center.org/loader.js', console.log('didomi loader.js loaded'));
+    }
 };
 
-if (env.isFlag('didomi')) {
-    init();
-}
+let attachEventListeners = function(): void {
+    window.didomiEventListeners = window.didomiEventListeners || [];
+    window.didomiEventListeners.push({
+        event: 'consent.changed',
+        listener: (context) => {
+            consentChanged$.fire(context);
+        }
+    });
+};
 
-// <script type="text/javascript" id="spcloader" src="" async></script>
+export let isConsentedPurpose = function(purpose: Purpose): boolean {
+    return window.Didomi.isConsentRequired() && window.Didomi.getUserConsentStatusForPurpose(purpose);
+};
+
+export let onChange = function(handler: (event: object) => void): void {
+    consentChanged$.observe(handler);
+};
