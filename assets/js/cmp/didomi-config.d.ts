23867c9fca42952b33a82c27cb1d6b0cae37cbd8
aleborgne <alex.e.leb@gmail.com>
2019-11-04 16:10:41 +0100

    fix didomi config - WEBDEV-4475

--- assets/js/cmp/didomi-config.d.ts
+++ assets/js/cmp/didomi-config.d.ts
@@ -31,88 +31,88 @@ export declare let didomiConfig: (options: DidomiOptions) => {
                 fr: string;
             };
         }[];
-    };
-    notice: {
-        enable: boolean;
-        position: string;
-        logoAlignment: string;
-        closeOnClick: boolean;
-        closeOnClickNavigationDelay: number;
-        closeOnClickBackdrop: boolean;
-    };
-    preferences: {
-        canCloseWhenConsentIsMissing: boolean;
-        showWhenConsentIsMissing: boolean;
-        enableAllButtons: boolean;
-        information: {
+        notice: {
             enable: boolean;
-            content: {
-                popup: {
-                    fr: string;
+            position: string;
+            logoAlignment: string;
+            closeOnClick: boolean;
+            closeOnClickNavigationDelay: number;
+            closeOnClickBackdrop: boolean;
+        };
+        preferences: {
+            canCloseWhenConsentIsMissing: boolean;
+            showWhenConsentIsMissing: boolean;
+            enableAllButtons: boolean;
+            information: {
+                enable: boolean;
+                content: {
+                    popup: {
+                        fr: string;
+                    };
+                    title: {
+                        fr: string;
+                    };
+                    text: {
+                        fr: string;
+                    };
+                    learnMore: {
+                        fr: string;
+                    };
+                    agreeAndClose: {
+                        fr: string;
+                    };
                 };
+            };
+            content: {
                 title: {
                     fr: string;
                 };
-                text: {
+                agreeToAll: {
                     fr: string;
                 };
-                learnMore: {
-                    fr: string;
-                };
-                agreeAndClose: {
+                disagreeToAll: {
                     fr: string;
                 };
             };
+            categories: {
+                type: string;
+                purposeId: string;
+            }[];
         };
-        content: {
-            title: {
-                fr: string;
-            };
-            agreeToAll: {
-                fr: string;
-            };
-            disagreeToAll: {
-                fr: string;
-            };
+        languages: {
+            enabled: string[];
+            default: string;
         };
-        categories: {
-            type: string;
-            purposeId: string;
-        }[];
-    };
-    languages: {
-        enabled: string[];
-        default: string;
-    };
-    theme: {
-        color: string;
-        linkColor: string;
-        font: string;
-        buttons: {
-            regularButtons: {
-                backgroundColor: string;
-                textColor: string;
-                borderColor: string;
-                borderWidth: string;
-                borderRadius: string;
-            };
-            highlightButtons: {
-                backgroundColor: string;
-                textColor: string;
-                borderColor: string;
-                borderWidth: string;
-                borderRadius: string;
+        theme: {
+            color: string;
+            linkColor: string;
+            font: string;
+            buttons: {
+                regularButtons: {
+                    backgroundColor: string;
+                    textColor: string;
+                    borderColor: string;
+                    borderWidth: string;
+                    borderRadius: string;
+                };
+                highlightButtons: {
+                    backgroundColor: string;
+                    textColor: string;
+                    borderColor: string;
+                    borderWidth: string;
+                    borderRadius: string;
+                };
             };
         };
-    };
-    tagManager: {
-        provider: string;
-    };
-    user: {
-        bots: {
-            consentRequired: boolean;
-            types: string[];
-            extraUserAgents: any[];
+        tagManager: {
+            provider: string;
+        };
+        user: {
+            bots: {
+                consentRequired: boolean;
+                types: string[];
+                extraUserAgents: any[];
+            };
         };
     };
 };
