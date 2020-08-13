a261d8a776e5b6bd4b7db43e3a44dd8e0027f896
nmatias <nicolasmatias.dev@gmail.com>
2020-08-13 17:50:30 +0200

    Revert Mise à jour de didomi TCF V2 RADIODIG-318

--- assets/js/cmp/didomi.js
+++ assets/js/cmp/didomi.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var env = require("../env/env");
 var $ = require("../../../assets/js/utils/dom");
+var scriptLoader = require("../../js/utils/script-loader.js");
 var cookies = require("js-cookie");
 var didomi_config_1 = require("./didomi-config");
+var didomi_css_1 = require("./didomi-css");
 var cnil_log_service_1 = require("../cnil/cnil-log-service");
 var cnil_log_1 = require("../cnil/cnil-log");
 var Purpose;
@@ -29,56 +31,111 @@ var CmpDidomi;
 (function (CmpDidomi) {
     CmpDidomi.init = function (options) {
         window.gdprAppliesGlobally = true;
-        options.apiKey = options.apiKey || didomi_config_1.DEFAULT_OPTIONS.apiKey;
-        // @ts-ignore
-        (function () { function a(e) { if (!window.frames[e]) {
-            if (document.body && document.body.firstChild) {
-                var t = document.body;
-                var n = document.createElement("iframe");
-                n.style.display = "none";
-                n.name = e;
-                n.title = e;
-                t.insertBefore(n, t.firstChild);
+        if (env.isFlag('tcfv2')) {
+            console.log("tcfv2");
+            options.apiKey = options.apiKey || didomi_config_1.DEFAULT_OPTIONS.apiKey;
+            // @ts-ignore
+            (function () { function a(e) { if (!window.frames[e]) {
+                if (document.body && document.body.firstChild) {
+                    var t = document.body;
+                    var n = document.createElement("iframe");
+                    n.style.display = "none";
+                    n.name = e;
+                    n.title = e;
+                    t.insertBefore(n, t.firstChild);
+                }
+                else {
+                    setTimeout(function () { a(e); }, 5);
+                }
+            } } function e(n, r, o, c, s) { function e(e, t, n, a) { if (typeof n !== "function") {
+                return;
+            } if (!window[r]) {
+                window[r] = [];
+            } var i = false; if (s) {
+                i = s(e, t, n);
+            } if (!i) {
+                window[r].push({ command: e, parameter: t, callback: n, version: a });
+            } } e.stub = true; function t(a) { if (!window[n] || window[n].stub !== true) {
+                return;
+            } if (!a.data) {
+                return;
+            } var i = typeof a.data === "string"; var e; try {
+                e = i ? JSON.parse(a.data) : a.data;
             }
-            else {
-                setTimeout(function () { a(e); }, 5);
-            }
-        } } function e(n, r, o, c, s) { function e(e, t, n, a) { if (typeof n !== "function") {
-            return;
-        } if (!window[r]) {
-            window[r] = [];
-        } var i = false; if (s) {
-            i = s(e, t, n);
-        } if (!i) {
-            window[r].push({ command: e, parameter: t, callback: n, version: a });
-        } } e.stub = true; function t(a) { if (!window[n] || window[n].stub !== true) {
-            return;
-        } if (!a.data) {
-            return;
-        } var i = typeof a.data === "string"; var e; try {
-            e = i ? JSON.parse(a.data) : a.data;
+            catch (t) {
+                return;
+            } if (e[o]) {
+                var r = e[o];
+                window[n](r.command, r.parameter, function (e, t) { var n = {}; n[c] = { returnValue: e, success: t, callId: r.callId }; a.source.postMessage(i ? JSON.stringify(n) : n, "*"); }, r.version);
+            } } if (typeof window[n] !== "function") {
+                window[n] = e;
+                if (window.addEventListener) {
+                    window.addEventListener("message", t, false);
+                }
+                else {
+                    window.attachEvent("onmessage", t);
+                }
+            } } e("__tcfapi", "__tcfapiBuffer", "__tcfapiCall", "__tcfapiReturn"); a("__tcfapiLocator"); })();
+            // @ts-ignore
+            (function (e) { var t = document.createElement("script"); t.id = "spcloader"; t.type = "text/javascript"; t.async = true; t.src = "https://sdk.privacy-center.org/" + e + "/loader.js?target=" + document.location.hostname; t.charset = "utf-8"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(t, n); })(options.apiKey);
+            CmpDidomi.attach('didomiOnReady', function (Didomi) {
+                console.log("didomiOnReady : trackConsent + logConsent");
+                CmpDidomi.trackConsent();
+                CmpDidomi.logConsent();
+            });
         }
-        catch (t) {
-            return;
-        } if (e[o]) {
-            var r = e[o];
-            window[n](r.command, r.parameter, function (e, t) { var n = {}; n[c] = { returnValue: e, success: t, callId: r.callId }; a.source.postMessage(i ? JSON.stringify(n) : n, "*"); }, r.version);
-        } } if (typeof window[n] !== "function") {
-            window[n] = e;
-            if (window.addEventListener) {
-                window.addEventListener("message", t, false);
+        else {
+            // @ts-ignore
+            (function () { function n() { if (!window.frames.__cmpLocator) {
+                if (document.body && document.body.firstChild) {
+                    var e = document.body;
+                    var t = document.createElement("iframe");
+                    t.style.display = "none";
+                    t.name = "__cmpLocator";
+                    e.insertBefore(t, e.firstChild);
+                }
+                else {
+                    setTimeout(n, 5);
+                }
+            } } function e(e, t, n) { if (typeof n !== "function") {
+                return;
+            } if (!window.__cmpBuffer) {
+                window.__cmpBuffer = [];
+            } if (e === "ping") {
+                n({ gdprAppliesGlobally: window.gdprAppliesGlobally, cmpLoaded: false }, true);
             }
             else {
-                window.attachEvent("onmessage", t);
+                window.__cmpBuffer.push({ command: e, parameter: t, callback: n });
+            } } e.stub = true; function t(a) { if (!window.__cmp || window.__cmp.stub !== true) {
+                return;
+            } if (!a.data) {
+                return;
+            } var r = typeof a.data === "string"; var e; try {
+                e = r ? JSON.parse(a.data) : a.data;
             }
-        } } e("__tcfapi", "__tcfapiBuffer", "__tcfapiCall", "__tcfapiReturn"); a("__tcfapiLocator"); })();
-        // @ts-ignore
-        (function (e) { var t = document.createElement("script"); t.id = "spcloader"; t.type = "text/javascript"; t.async = true; t.src = "https://sdk.privacy-center.org/" + e + "/loader.js?target=" + document.location.hostname; t.charset = "utf-8"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(t, n); })(options.apiKey);
-        CmpDidomi.attach('didomiOnReady', function (Didomi) {
-            console.log("didomiOnReady : trackConsent + logConsent");
-            CmpDidomi.trackConsent();
-            CmpDidomi.logConsent();
-        });
+            catch (t) {
+                return;
+            } if (e.__cmpCall) {
+                var i = e.__cmpCall;
+                window.__cmp(i.command, i.parameter, function (e, t) { var n = { __cmpReturn: { returnValue: e, success: t, callId: i.callId } }; a.source.postMessage(r ? JSON.stringify(n) : n, "*"); });
+            } } if (typeof window.__cmp !== "function") {
+                window.__cmp = e;
+                if (window.addEventListener) {
+                    window.addEventListener("message", t, false);
+                }
+                else {
+                    window.attachEvent("onmessage", t);
+                }
+            } n(); })();
+            window.didomiConfig = didomi_config_1.didomiConfig(options);
+            var style = document.createElement('style');
+            style.innerHTML = didomi_css_1.didomiCustomCss(options);
+            document.head.appendChild(style);
+            scriptLoader.ensureLoaded('https://sdk.privacy-center.org/loader.js').then(function () {
+                CmpDidomi.trackConsent();
+                CmpDidomi.logConsent();
+            });
+        }
     };
     CmpDidomi.logConsent = function () {
         CmpDidomi.attach('didomiEventListeners', {
@@ -164,7 +221,12 @@ var CmpDidomi;
         }
     };
     CmpDidomi.toPurposeId = function (purpose) {
-        var ids = RTLPurposeIds;
-        return ids[purpose];
+        if (env.isFlag('tcfv2')) {
+            var ids = env.getDomain() == 'funradio.fr' ? FunRadioPurposeIds : RTLPurposeIds;
+            return ids[purpose];
+        }
+        else {
+            return purpose;
+        }
     };
 })(CmpDidomi = exports.CmpDidomi || (exports.CmpDidomi = {}));
